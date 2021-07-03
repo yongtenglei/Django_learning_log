7a77e096c5b43357c5d22950256ac66d8cc1fa5c
rey <3065588496@qq.com>
2021-07-04 01:12:25 +0800

    Correct a typo

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -139,5 +139,5 @@ import django_heroku
 django_heroku.settings(locals())
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
-elif os.environ.get('DEBUG') == 'TALSE':
+elif os.environ.get('DEBUG') == 'FALSE':
     DEBUG = False
